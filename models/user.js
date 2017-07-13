4fc4fc3a0b486529cfe1153a35a614e7650aaf95
Yoana Georgieva <yo.ana.georgieva@abv.bg>
2017-07-13 16:16:34 +0300

    Auto stash before merge of "master" and "origin/master"

--- models/user.js
+++ models/user.js
@@ -71,3 +71,71 @@ class User {
 }
 
 module.exports = User;
+
+    create(username, password, firstName, lastName, email, age) {
+        //validation
+        this.collection.insert({
+            username: username,
+            password: password,
+            firstName: firstName,
+            lastName: lastName,
+            email: email,
+            age: age,
+            events: [],
+        });
+    }
+
+    getEvents(username) {
+        return this.collection
+            .find({ username: username })
+            .toArray((err, user) => {
+                if (user.length === 0) {
+                    throw new Error('Invalid user!');
+                } else {
+                    return user.events;
+                }
+            });
+    }
+
+    addEvent(username, event) {
+        this.collection.find({ username: username })
+            .toArray((err, user) => {
+                if (user.length === 0) {
+                    throw new Error('Invalid user!');
+                } else {
+                    user.events.push(event);
+                }
+            });
+    }
+
+    login(username, password) {
+        return this.collection
+            .find({ username: username, password: password })
+            .toArray((err, user) => {
+                if (user.length === 0) {
+                    throw new Error('Invalid user!');
+                } else {
+                    return user;
+                }
+            });
+    }
+
+    getProfile(username) {
+        return this.collection
+            .find({ username: username })
+            .toArray((err, user) => {
+                if (user.length === 0) {
+                    throw new Error('Invalid user!');
+                } else {
+                    return user;
+                }
+            });
+    }
+
+    updateProfile(username, firstName, lastName, email, age) {
+        this.collection
+            .update({ username: username }, { $set: { firstName: firstName, lastName: lastName, age: age, email: email } });
+    }
+}
+
+module.exports = User;
