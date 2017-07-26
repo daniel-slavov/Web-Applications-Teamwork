e9089045816277603ac8901694975966e3a5360b
Simona Tsenova <simona.tsenova17@gmail.com>
2017-07-26 14:33:09 +0300

    Deleted unnecessary stuff from file

--- public/js/deletion.js
+++ public/js/deletion.js
@@ -4,12 +4,11 @@ const deletion = () => {
         console.log(title);
         let url = `${document.location.origin}/api/events/${title}`;
         console.log('URL: ' + url);
-=======
+
         if (confirm("Please confirm that you want to delete the event.")) {
             requester.delete(url)
                  .then(response => {
                      location.href = response.redirect;
->>>>>>> b6051c24690758f7e4a60740849c42b1fcdd5bd4
                  });
         }
     });
